6956213c84c6f692955a05bdb2e620127ab72c64
João Pedro <joaosanttos528@gmail.com>
2025-11-24 19:16:54 +0000

    Na página de relatar um problema tem dois cabeçalhos superiores, deixe u

--- src/app/report/new/page.tsx
+++ src/app/report/new/page.tsx
@@ -1,10 +1,9 @@
-import { Header } from "@/components/header";
+
 import { ReportForm } from "@/components/report-form";
 
 export default function NewReportPage() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Header />
       <main className="flex-1 p-4 md:p-8 flex items-start justify-center">
         <ReportForm />
       </main>
