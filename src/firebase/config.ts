83ed17e174e86302adbc512bfd882544006e55e3
João Pedro <joaosanttos528@gmail.com>
2025-11-25 19:04:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1742534524-1bd2f",
+  "appId": "1:144570252869:web:fc914ae039fd8944e32d2c",
+  "apiKey": "AIzaSyAEhzXkUyfgZOmiyqr_WVlsd3Xe9qsa6eM",
+  "authDomain": "studio-1742534524-1bd2f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "144570252869"
+};
